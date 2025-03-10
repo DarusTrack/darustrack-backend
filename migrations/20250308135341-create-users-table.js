51c538ee5fa22120f9a0d894dad4ab92401aaebd
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-03-10 21:37:44 +0700

    add validate isEmail on migrate

--- migrations/20250308135341-create-users-table.js
+++ migrations/20250308135341-create-users-table.js
@@ -16,7 +16,10 @@ module.exports = {
       email: {
         type: Sequelize.STRING,
         allowNull: false,
-        unique: true
+        unique: true, 
+        validate: {
+          isEmail: true
+        }
       },
       password: {
         type: Sequelize.STRING,
