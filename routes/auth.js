91e5d060241ae036c5521830a15bfc576e8a77d4
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-03-10 23:02:16 +0700

    feat: route post logout, get and put profile

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,6 @@ const bcrypt = require("bcrypt");
 const { User } = require("../models");
 const accessValidation = require("../middleware/accessValidation");
 const Validator = require("fastest-validator");
-
 require("dotenv").config();
 const v = new Validator();
 
@@ -13,7 +12,6 @@ const v = new Validator();
 router.post("/login", async (req, res) => {
     const { email, password } = req.body;
 
-    // Validasi input tidak boleh kosong
     if (!email || !password) {
         return res.status(400).json({ message: "Email dan password harus diisi" });
     }
@@ -24,13 +22,11 @@ router.post("/login", async (req, res) => {
             return res.status(400).json({ message: "Invalid email or password" });
         }
 
-        // Bandingkan password yang dimasukkan dengan hash di database
         const validPassword = await bcrypt.compare(password, user.password);
         if (!validPassword) {
             return res.status(400).json({ message: "Invalid email or password" });
         }
 
-        // Buat token JWT
         const token = jwt.sign(
             { id: user.id, name: user.name, role: user.role },
             process.env.JWT_SECRET,
@@ -44,7 +40,34 @@ router.post("/login", async (req, res) => {
     }
 });
 
-// Get Profile (hanya user yang login)
+router.post("/logout", accessValidation, (req, res) => {
+    res.json({ message: "Logout successful" });
+});
+
+// Update Profile (Hanya bisa dilakukan oleh user yang login)
+router.put("/profile", accessValidation, async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        const user = await User.findByPk(req.user.id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        // Update data pengguna
+        if (name) user.name = name;
+        if (email) user.email = email;
+        if (password) user.password = await bcrypt.hash(password, 10);
+
+        await user.save();
+        res.json({ message: "Profile updated successfully", user });
+    } catch (error) {
+        console.error("Update Profile Error:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+// Get Profile (Hanya bisa dilakukan oleh user yang login)
 router.get("/profile", accessValidation, async (req, res) => {
     try {
         const user = await User.findByPk(req.user.id, {
