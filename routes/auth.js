9b15b23c4536c1d6391a6e0232c8458f33b08ef9
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-03-19 23:05:28 +0700

    fix: bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { User } = require("../models");
 const { accessValidation, tokenBlacklist } = require("../middlewares/accessValidation");
 const Validator = require("fastest-validator");
