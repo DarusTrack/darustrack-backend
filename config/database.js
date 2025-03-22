4f3cd0dd004fdc04f4d1212c16d89e460014dc03
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-03-22 17:07:24 +0700

    feat: npm install

--- config/database.js
+++ config/database.js
@@ -1,15 +1,11 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'mysql',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT,
+  logging: false // Matikan logging jika tidak ingin banyak output di terminal
 });
 
-sequelize.authenticate()
-  .then(() => console.log('Connected to MySQL'))
-  .catch(err => console.error('Connection error:', err));
+module.exports = sequelize;
