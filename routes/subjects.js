cc8ebeb2089e4f9e5d69a5e8ac4fd4fa4420d8ff
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-04-06 17:20:32 +0700

    fix: put subjects

--- routes/subjects.js
+++ routes/subjects.js
@@ -24,7 +24,7 @@ router.get("/", async (req, res) => {
 router.post('/', async (req, res) => {
     const schema = {
         name: 'string',
-        description: 'text'
+        description: 'string'
     };
 
     const validate = v.validate(req.body, schema);
