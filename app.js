0a0732077ec9a47e454ef0b027a5b45020813be5
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-04-06 21:46:31 +0700

    fix: app.js

--- app.js
+++ app.js
@@ -26,8 +26,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
+  origin: ['http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 }));
 
 app.use('/', indexRouter);
