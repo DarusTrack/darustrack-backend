e694d910cf8c77eb1ba2071b37e8fd5df41b2ca4
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-04-06 21:50:14 +0700

    fix: app.js

--- app.js
+++ app.js
@@ -25,12 +25,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors({
-  origin: ['http://localhost:3000'],
+const cors = require('cors');
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://darustrack-frontend-production.up.railway.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
-}));
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
