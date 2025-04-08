fc7ef9c13c03904580cdca20b4e9a5191c71c0c5
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-04-08 07:07:25 +0700

    fix: cors

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
 require('dotenv').config();
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var authRouter = require('./routes/auth');
-var usersRouter = require('./routes/users');
-var teachersRouter = require('./routes/teachers');
-var parentsRouter = require('./routes/parents');
-var headMasterRouter = require('./routes/headmaster');
-var classesRouter = require('./routes/classes');
-var curriculumsRouter = require('./routes/curriculums');
-var subjectsRouter = require('./routes/subjects');
-var academicCalendarRouter = require('./routes/academic_calendar');
+const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
+const usersRouter = require('./routes/users');
+const teachersRouter = require('./routes/teachers');
+const parentsRouter = require('./routes/parents');
+const headMasterRouter = require('./routes/headmaster');
+const classesRouter = require('./routes/classes');
+const curriculumsRouter = require('./routes/curriculums');
+const subjectsRouter = require('./routes/subjects');
+const academicCalendarRouter = require('./routes/academic_calendar');
 
-var app = express();
+const app = express();
 
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://darustrack-frontend.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
+// Debug header CORS
+app.use((req, res, next) => {
+  console.log("CORS headers:", res.getHeaders());
+  next();
+});
+
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://darustrack-frontend.vercel.app/'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
-
-app.use((req, res, next) => {
-  console.log("CORS headers:", res.getHeaders());
-  next();
-});
-
+// Routing
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
@@ -48,10 +51,11 @@ app.use('/curriculums', curriculumsRouter);
 app.use('/subjects', subjectsRouter);
 app.use('/academic-calendar', academicCalendarRouter);
 
+// Database connection
 const sequelize = require('./config/database');
 
 sequelize.authenticate()
-  .then(() => console.log('Database connected...'))
-  .catch(err => console.error('Error connecting to database:', err));
+  .then(() => console.log('✅ Database connected...'))
+  .catch(err => console.error('❌ Error connecting to database:', err));
 
 module.exports = app;
