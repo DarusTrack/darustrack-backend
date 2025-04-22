3bf88f9c9832c347baf70a66407e54aa817470a6
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-04-22 12:47:28 +0700

    change: cors

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://darustrack-frontend.vercel.app'
+  'https://darustrack.vercel.app'
 ];
 
 const corsOptions = {
