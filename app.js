2b7c54a9183f87a534260bc843e3e383cc67469b
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-05-01 08:12:22 +0700

    fix error railway

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.options('*', cors(corsOptions)); // untuk preflight
 // Middleware
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
