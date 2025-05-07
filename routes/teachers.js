588e42b380fc176bdd152a79ed4daab12cc6770e
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-05-07 16:51:06 +0700

    fix: routes get categories teachers

--- routes/teachers.js
+++ routes/teachers.js
@@ -733,11 +733,12 @@ router.get('/grades/:semesterId/:subjectId/categories', async (req, res) => {
             include: {
                 model: AcademicYear,
                 as: 'academic_year',
+                where: { is_active: true },
                 attributes: ['id', 'year', 'is_active']
             }
         });
 
-        if (!semester || !semester.academic_year || !semester.academic_year.is_active) {
+        if (!semester) {
             return res.status(404).json({ message: 'Semester tidak ditemukan atau tidak berada di tahun ajaran aktif' });
         }
 
