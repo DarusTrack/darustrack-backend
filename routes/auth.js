8425c0733be9442dd660b82ab19fdbd14a28456c
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-05-19 04:42:01 +0700

    feat: testing

--- routes/auth.js
+++ routes/auth.js
@@ -56,7 +56,7 @@ router.post("/login", async (req, res) => {
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
-        res.status(200).json({ message: "Login successful", accessToken, expiresIn });
+        res.status(200).json({ message: "Login successful", accessToken });
     } catch (error) {
         res.status(500).json({ message: "Internal server error", error: error.message });
     }
