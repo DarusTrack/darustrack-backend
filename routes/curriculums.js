9bc3b0f265797acf877e5c8afe8d5aebf0359da8
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-05-22 05:44:36 +0700

    feat: contoller curriculum

--- routes/curriculums.js
+++ routes/curriculums.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const curriculumController = require('../controllers/curriculumController');
-const accessValidation = require('../middlewares/accessValidation');
 const roleValidation = require('../middlewares/roleValidation');
 
 // Get data kurikulum (hanya satu yang tersedia)
-router.get('/', accessValidation, curriculumController.getCurriculum);
+router.get('/', curriculumController.getCurriculum);
 
-router.put('/:id', accessValidation, roleValidation(["admin"]), curriculumController.updateCurriculum);
+router.put('/:id', roleValidation(["admin"]), curriculumController.updateCurriculum);
 
 module.exports = router;
