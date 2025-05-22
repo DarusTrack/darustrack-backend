3d1ef2ecacabe14581de40f2900534a44d4d8f39
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-05-22 07:52:17 +0700

    feat: user controller

--- routes/users.js
+++ routes/users.js
@@ -1,42 +1,21 @@
 var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/userController');
-const roleValidation = require("../middlewares/roleValidation");
-const accessValidation = require('../middlewares/accessValidation');
 
 // Get all users with optional role filter
-router.get('/', 
-    accessValidation, 
-    roleValidation(["admin"]), 
-    userController.getAllUsers
-);
+router.get('/', userController.getAllUsers);
 
 // Get single user by ID
-router.get('/:id', 
-    accessValidation, 
-    roleValidation(["admin"]), 
-    userController.getUserById
-);
+router.get('/:id', userController.getUserById);
 
 // Create new user
-router.post('/', 
-    accessValidation, 
-    roleValidation(["admin"]), 
-    userController.createUser
-);
+router.post('/', userController.createUser);
 
 // Update user
-router.put('/:id',  
-    accessValidation, 
-    roleValidation(["admin"]), 
-    userController.updateUser
+router.put('/:id', userController.updateUser
 );
 
 // Delete user
-router.delete('/:id',  
-    accessValidation, 
-    roleValidation(["admin"]), 
-    userController.deleteUser
-);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
