e09d99c4deaeb99a30132e9fbcd69b83b6760de4
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-05-22 07:40:10 +0700

    fix: argon2

--- models/User.js
+++ models/User.js
@@ -1,62 +1,65 @@
-const bcrypt = require('bcryptjs');
+const argon2 = require('argon2');
 const { customAlphabet } = require('nanoid');
 const nanoid = customAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', 5);
 
-const BCRYPT_COST_FACTOR = 8; // Semula 10
-
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-        id: {
-            type: DataTypes.STRING(5),
-            // defaultValue: Sequelize.UUIDV4,
-            primaryKey: true,
-            allowNull: false,
-            defaultValue: () => nanoid()
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        nip: {
-            type: DataTypes.STRING,
-            allowNull: true,
-            unique: true
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true, 
-            validate: {
-                isEmail: true
-            }
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        role: {
-            type: DataTypes.ENUM('orang_tua', 'kepala_sekolah', 'wali_kelas', 'admin'),
-            allowNull: false
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.STRING(5),
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: () => nanoid()
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    nip: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: { isEmail: true }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.ENUM('orang_tua', 'kepala_sekolah', 'wali_kelas', 'admin'),
+      allowNull: false
+    }
+  }, {
+    tableName: 'users',
+    hooks: {
+      beforeCreate: async (user) => {
+        user.password = await argon2.hash(user.password);
+      },
+      beforeUpdate: async (user) => {
+        if (user.changed('password')) {
+          user.password = await argon2.hash(user.password);
         }
-    }, {
-        tableName: 'users',
-        hooks: {
-            beforeCreate: async (user) => {
-                user.password = await bcrypt.hash(user.password, BCRYPT_COST_FACTOR);
-            }
-        }
-    });
+      }
+    }
+  });
 
-    // Method untuk compare password yang dioptimasi
-    User.prototype.comparePassword = async function(candidatePassword) {
-        return bcrypt.compare(candidatePassword, this.password);
-    };
+  User.prototype.comparePassword = async function(candidatePassword) {
+    try {
+      return await argon2.verify(this.password, candidatePassword);
+    } catch {
+      return false;
+    }
+  };
 
-    User.associate = (models) => {
-        User.hasMany(models.Student, { foreignKey: 'parent_id', as: 'student' });
-        User.hasOne(models.Class, { foreignKey: 'teacher_id', as: 'class' });
-        User.hasMany(models.PasswordReset, { foreignKey: 'user_id' });
-    };
+  User.associate = (models) => {
+    User.hasMany(models.Student, { foreignKey: 'parent_id', as: 'student' });
+    User.hasOne(models.Class, { foreignKey: 'teacher_id', as: 'class' });
+    User.hasMany(models.PasswordReset, { foreignKey: 'user_id' });
+  };
 
-    return User;
+  return User;
 };
